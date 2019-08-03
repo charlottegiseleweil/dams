f8fac203643eedd827d470f523671aa45408b8ae
Ubuntu <adminatcap@beaverdam.ctzfzb2eeoculmajp4deuspuye.xx.internal.cloudapp.net>
2019-08-03 01:57:07 +0000

    create_csv update

--- evaluation/create_csv.py
+++ evaluation/create_csv.py
@@ -105,22 +105,27 @@ def calc_IoU (ground_truth, predicted):
 	Returns:
 		float: value of the IoU for the two boxes.
 	"""
+
 	# gather gt and pred coords
 	x1_t, y1_t, x2_t, y2_t = ground_truth
 	x1_p, y1_p, x2_p, y2_p = predicted
-	# find extremes
-	far_x = np.min([x2_t, x2_p])
-	near_x = np.max([x1_t, x1_p])
-	far_y = np.min([y2_t, y2_p])
-	near_y = np.max([y1_t, y1_p])
-	# calc areas
-	inter_area = (far_x - near_x + 1) * (far_y - near_y + 1)
-	gt_box_area = (x2_t - x1_t + 1) * (y2_t - y1_t + 1)
-	pred_box_area = (x2_p - x1_p + 1) * (y2_p - y1_p + 1)
-	# calc IoU
-	iou = inter_area / (gt_box_area + pred_box_area - inter_area)
-	iou = float('%.4f'%(iou))
-	return iou
+	# make sure the bounding boxes overlap
+	if (x2_p < x1_t) | (x2_t < x1_p):
+		return 0
+	else:
+		# find extremes
+		far_x = np.min([x2_t, x2_p])
+		near_x = np.max([x1_t, x1_p])
+		far_y = np.min([y2_t, y2_p])
+		near_y = np.max([y1_t, y1_p])
+		# calc areas
+		inter_area = (far_x - near_x + 1) * (far_y - near_y + 1)
+		gt_box_area = (x2_t - x1_t + 1) * (y2_t - y1_t + 1)
+		pred_box_area = (x2_p - x1_p + 1) * (y2_p - y1_p + 1)
+		# calc IoU
+		iou = inter_area / (gt_box_area + pred_box_area - inter_area)
+		iou = float('%.4f'%(iou))
+		return iou
 
 # create dictionaries of ground_truth bboxes,  predicted bboxes, and confidence
 ground_truth_dict = {}
