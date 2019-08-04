b1c8dc7bcf271095652c94426ee33a2015dfe683
Ubuntu <adminatcap@beaverdam.ctzfzb2eeoculmajp4deuspuye.xx.internal.cloudapp.net>
2019-08-04 04:00:12 +0000

    updates

--- evaluation/create_csv.py
+++ evaluation/create_csv.py
@@ -169,11 +169,11 @@ detect_df['iou'] = detect_df['image'].map(pd.Series(iou_dict))
 print('added iou')
 
 # add tp, fp, and fn
-detect_df['tp@IoU'+str(iou_thres)] = np.where((detect_df.iou >= iou_thres), '1', '0')
+detect_df['tp@IoU'+str(iou_thres)] = np.where((detect_df.iou > iou_thres), '1', '0')
 #detect_df['tp@IoU'+str(iou_thres)] = np.where(((detect_df.predicted.notnull()) & (detect_df.iou >= iou_thres)) & detect_df.ground_truth.notnull(), '1', '0')
-detect_df['fp@IoU'+str(iou_thres)] = np.where((detect_df.iou < iou_thres) | (detect_df.predicted.isnull() & detect_df.ground_truth.notnull()), '1', '0')
+detect_df['fp@IoU'+str(iou_thres)] = np.where((detect_df.iou <= iou_thres) | (detect_df.predicted.isnull() & detect_df.ground_truth.notnull()), '1', '0')
 #detect_df['fp@IoU'+str(iou_thres)] = np.where(detect_df.predicted.notnull() & detect_df.ground_truth.isnull(), '1', '0')
-detect_df['fn@IoU'+str(iou_thres)] = np.where((detect_df.iou < iou_thres) | (detect_df.predicted.notnull() & detect_df.ground_truth.isnull()), '1', '0')
+detect_df['fn@IoU'+str(iou_thres)] = np.where((detect_df.iou <= iou_thres) | (detect_df.predicted.notnull() & detect_df.ground_truth.isnull()), '1', '0')
 #detect_df['fn@IoU'+str(iou_thres)] = np.where(((detect_df.predicted.isnull()) | (detect_df.iou < iou_thres)) & detect_df.ground_truth.notnull(), '1', '0')
 print(detect_df)
 
